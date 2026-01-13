0e1087115124aa50e8d2691a7cb6c3ccc2605232
lportuano <luchoportuano460@gmail.com>
2026-01-13 17:18:51 -0500

    Navegacion Botton Roy

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StackNav from './navigations/StackNavigator';
+import TopNav from './navigations/BottonNavigator';
 
 export default function App() {
   return (
-    <StackNav/>
+    <TopNav/>
   );
 }
 
